f5f9f6d48246efe48f18a17bf85bfbee7856279e
Carlos Verdes <cverdes@gmail.com>
2022-12-28 22:35:36 +0400

    update zio-json dependency

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
     val zioConfigV = "3.0.2"
     val zioHttpV = "0.0.3"
     val zioLoggingV = "2.1.0"
-    val zioJsonV = "0.3.0"
+    val zioJsonV = "0.4.2"
     val zioConfMagnoliaV = "3.0.2"
     val zioConfTypesafeV = "3.0.2"
     val zioCryptoV = "0.0.1"
