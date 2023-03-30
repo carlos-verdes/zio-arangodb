842bb91f0986021af72629f508b9c7d96cbb2489
Scala Steward <scala_steward@virtuslab.com>
2023-03-30 14:20:00 +0000

    Update testcontainers-scala-core to 0.40.14

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,7 +18,7 @@ object Dependencies {
     val logBackV = "1.4.6"
     val scalaUriV = "4.0.2"
     val scodecV = "2.2.0"
-    val testContainersV = "0.40.10"
+    val testContainersV = "0.40.14"
   }
 
   object Libraries {
